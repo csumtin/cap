817f652f41466b34fe88255e7decdf7bd1793ba5
csumtin <csumtin@protonmail.com>
2022-04-24 18:12:01 +0100

    oops

--- google-chrome/init.sh
+++ google-chrome/init.sh
@@ -9,9 +9,17 @@ if [[ $EUID -ne 0  ]]; then
   exit 1
 fi
 
+mkdir bootstrap
 
+debootstrap --arch=amd64 --variant=minbase stable bootstrap
 
-systemd-nspawn -D bootstrap  /bin/bash <<EOT
+echo "Pick root password"
+systemd-nspawn -D bootstrap passwd
+
+echo "Add user c and pick password"
+systemd-nspawn -D bootstrap adduser c
+
+systemd-nspawn -D bootstrap -P /bin/bash <<EOT
 DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends wget ca-certificates
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
