a2b0e0f548d209996138ac4906a38be083991193
csumtin <csumtin@protonmail.com>
2022-04-25 19:24:32 +0000

    set ephemeral by default

--- gedit/start.sh
+++ gedit/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eux
 cd "$(dirname "$0")"
-sudo systemd-nspawn -D bootstrap -u c -M gedit --setenv=DISPLAY=$DISPLAY --bind-ro=/tmp/.X11-unix --bind=/home/c/download --bind=/home/c/proj/cap --bind=/home/c/proj/cos --private-network gedit
+sudo systemd-nspawn -D bootstrap -u c -M gedit --setenv=DISPLAY=$DISPLAY --bind-ro=/tmp/.X11-unix --bind=/home/c/download --bind=/home/c/proj/cap --bind=/home/c/proj/cos --private-network --ephemeral gedit
