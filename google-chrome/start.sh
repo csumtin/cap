761378970d7e66b165fca90e351eac1a50ff4f85
csumtin <csumtin@protonmail.com>
2022-04-26 09:22:11 +0000

    ephemeral chrome

--- google-chrome/start.sh
+++ google-chrome/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eux
 cd "$(dirname "$0")"
-sudo systemd-nspawn -D bootstrap -u c -M google-chrome --setenv=DISPLAY=$DISPLAY --bind-ro=/tmp/.X11-unix google-chrome
+sudo systemd-nspawn -D bootstrap -u c -M google-chrome --setenv=DISPLAY=$DISPLAY --bind-ro=/tmp/.X11-unix --bind=/home/c/download --ephemeral google-chrome
